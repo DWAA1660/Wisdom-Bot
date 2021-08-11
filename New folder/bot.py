68080156a180408afaa0c597796ef60aae9d076d
DWAA1660 <80738438+DWAA1660@users.noreply.github.com>
2021-08-11 07:45:37 -0600

    Add files via upload

---
+++ New folder/bot.py
@@ -0,0 +1,21 @@
+import discord
+import os
+import random
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('We have loggon in as {0.user}'
+  .format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  
+
+  if message.content.startswith('$wisdom'):
+    with open('./resources/file.txt', 'r', encoding="utf8") as file:
+      await message.channel.send(random.choice(file.readlines()))
+client.run('ODc0NzAyMDI4MDIxMDU1NDk4.YRKzvg.n8XHV8zmNelRA_UC2VxtoODLn2k')
+
